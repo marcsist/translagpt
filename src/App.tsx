a2b0430879add2727a28c5f180a2b8739433ff78
Marcs <43764595+marcsist@users.noreply.github.com>
2025-07-13 17:34:01 +0200

    Remove header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import Header from './components/Header'
 import TranslationInterface from './components/TranslationInterface'
 
 function App() {
   return (
     <div className="min-h-screen bg-white flex flex-col">
-      <Header />
       <TranslationInterface />
     </div>
   )
